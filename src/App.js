bee41fcacb9b3fddcaceb777d9d4e23bfb1fefe4
ehlee95 <eric95lee@gmail.com>
2022-11-18 10:11:06 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,274 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import stations from "./stations";
+
+// fake data generator
+const getItems = (count, offset = 0) =>
+  Array.from({ length: count }, (v, k) => k).map((k) => ({
+    id: `item-${k + offset}-${new Date().getTime()}`,
+    content: `item ${k + offset}`,
+  }));
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+/**
+ * Moves an item from one list to another list.
+ */
+const move = (source, destination, droppableSource, droppableDestination) => {
+  const sourceClone = Array.from(source);
+  const destClone = Array.from(destination);
+  const [removed] = sourceClone.splice(droppableSource.index, 1);
+
+  destClone.splice(droppableDestination.index, 0, removed);
+
+  const result = {};
+  result[droppableSource.droppableId] = sourceClone;
+  result[droppableDestination.droppableId] = destClone;
+
+  console.log(result);
+  return result;
+};
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? "lightgreen" : "grey",
+
+  // styles we need to apply on draggables
+  ...draggableStyle,
+});
+const getListStyle = (isDraggingOver) => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: grid,
+  width: 250,
+});
 
 function App() {
+  const [state, setState] = useState([getItems(10), getItems(5, 10)]);
+
+  function onDragEnd(result) {
+    const { source, destination } = result;
+
+    // dropped outside the list
+    if (!destination) {
+      return;
+    }
+    const sInd = +source.droppableId;
+    const dInd = +destination.droppableId;
+
+    if (sInd === dInd) {
+      const items = reorder(state[sInd], source.index, destination.index);
+      const newState = [...state];
+      newState[sInd] = items;
+      setState(newState);
+    } else {
+      const result = move(state[sInd], state[dInd], source, destination);
+      const newState = [...state];
+      newState[sInd] = result[sInd];
+      newState[dInd] = result[dInd];
+
+      setState(newState.filter((group) => group.length));
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ display: "flex" }}>
+        <DragDropContext onDragEnd={onDragEnd}>
+          {state.map((el, ind) => (
+            <Droppable key={ind} droppableId={`${ind}`}>
+              {(provided, snapshot) => (
+                <div
+                  ref={provided.innerRef}
+                  style={getListStyle(snapshot.isDraggingOver)}
+                  {...provided.droppableProps}
+                >
+                  {el.map((item, index) => (
+                    <Draggable
+                      key={item.id}
+                      draggableId={item.id}
+                      index={index}
+                    >
+                      {(provided, snapshot) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          style={getItemStyle(
+                            snapshot.isDragging,
+                            provided.draggableProps.style
+                          )}
+                        >
+                          <div
+                            style={{
+                              display: "flex",
+                              justifyContent: "space-around",
+                            }}
+                          >
+                            {item.content}
+                          </div>
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          ))}
+        </DragDropContext>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+//   const [allStations, setAllStations] = useState(stations);
+
+//   function handleOnDragEnd(result) {
+//     console.log(result);
+//     if (!result.destination) return;
+//     const items = Array.from(allStations);
+//     const [reorderedItem] = items.splice(result.source.index, 1);
+//     items.splice(result.destination.index, 0, reorderedItem);
+//     setAllStations(items);
+//   }
+
+//   return (
+//     <div className="App">
+//       <div className="cols">
+//         <DragDropContext onDragEnd={handleOnDragEnd}>
+//           <Droppable droppableId="stationcards1">
+//             {(provided) => (
+//               <ul
+//                 {...provided.droppableProps}
+//                 ref={provided.innerRef}
+//                 className="Container1"
+//               >
+//                 {allStations.map(({ id, call_sign }, index) => {
+//                   return (
+//                     <Draggable
+//                       key={id.toString()}
+//                       draggableId={id.toString()}
+//                       index={index}
+//                     >
+//                       {(provided) => (
+//                         <li
+//                           className="Card"
+//                           ref={provided.innerRef}
+//                           {...provided.draggableProps}
+//                           {...provided.dragHandleProps}
+//                         >
+//                           {call_sign}
+//                         </li>
+//                       )}
+//                     </Draggable>
+//                   );
+//                 })}
+//                 {provided.placeholder}
+//               </ul>
+//             )}
+//           </Droppable>
+//           <Droppable droppableId="stationcards2">
+//             {(provided) => (
+//               <ul
+//                 {...provided.droppableProps}
+//                 ref={provided.innerRef}
+//                 className="Container2"
+//               >
+//                 {allStations.map(({ id, call_sign }, index) => {
+//                   return (
+//                     <Draggable
+//                       key={(id + 5).toString()}
+//                       draggableId={(id + 5).toString()}
+//                       index={index}
+//                     >
+//                       {(provided) => (
+//                         <li
+//                           className="Card"
+//                           ref={provided.innerRef}
+//                           {...provided.draggableProps}
+//                           {...provided.dragHandleProps}
+//                         >
+//                           {call_sign}
+//                         </li>
+//                       )}
+//                     </Draggable>
+//                   );
+//                 })}
+//                 {provided.placeholder}
+//               </ul>
+//             )}
+//           </Droppable>
+//         </DragDropContext>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// class MultipleDragList extends Component {
+//   state = {
+//       items: getItems(10),
+//       selected: getItems(5, 10)
+//   };
+
+//   // Defining unique ID for multiple lists
+//   id2List = {
+//       droppable: 'items',
+//       droppable2: 'selected'
+//   };
+
+//   getList = id => this.state[this.id2List[id]];
+
+//   onDragEnd = result => {
+//       const { source, destination } = result;
+
+//       if (!destination) {
+//           return;
+//       }
+
+//       // Sorting in same list
+//       if (source.droppableId === destination.droppableId) {
+//           const items = reorder(
+//               this.getList(source.droppableId),
+//               source.index,
+//               destination.index
+//           );
+
+//           let state = { items };
+
+//           if (source.droppableId === 'droppable2') {
+//               state = { selected: items };
+//           }
+
+//           this.setState(state);
+//       }
+//       // Interlist movement
+//       else {
+//           const result = move(
+//               this.getList(source.droppableId),
+//               this.getList(destination.droppableId),
+//               source,
+//               destination
+//           );
+
+//           this.setState({
+//               items: result.droppable,
+//               selected: result.droppable2
+//           });
+//       }
+//   };
---
+++ src/stations.js
@@ -0,0 +1,82 @@
+const stations = [
+  {
+    id: 1,
+    call_sign: "WXYC",
+    broadcast_frequency: "89.3",
+    audio_url: "https://audio-mp3.ibiblio.org/wxyc.mp3",
+    station_url: "https://wxyc.org/",
+    college_name: "University of North Carolina at Chapel Hill",
+    public_private: "public",
+    city: "Chapel Hill",
+    state: "NC",
+    station_image: "https://radiostationusa.fm/assets/image/radio/180/WXYC.jpg",
+    college_image:
+      "https://assets-sports.thescore.com/basketball/team/756/logo.png",
+    palette: ["#a4c0e4", "#263663", "#5e6687", "#6c7491"],
+  },
+  {
+    id: 2,
+    call_sign: "KALX",
+    broadcast_frequency: "90.7",
+    audio_url: "https://stream.kalx.berkeley.edu:8443/kalx-128.mp3",
+    station_url: "https://www.kalx.berkeley.edu/",
+    college_name: "University of California, Berkeley",
+    public_private: "public",
+    city: "Berkeley",
+    state: "CA",
+    station_image:
+      "https://www.kalx.berkeley.edu/sites/default/files/styles/large/public/KALX-55.png?itok=RaHLCXkA",
+    college_image: "https://wwll.com/images/logos/teams/cal-256.png",
+    palette: ["#092f63", "#f2c134", "#fcf9f3", "#4e566c"],
+  },
+  {
+    id: 3,
+    call_sign: "KVRX",
+    broadcast_frequency: "91.7",
+    audio_url: "https://www.kvrx.org/now_playing/stream",
+    station_url: "https://kvrx.org/app/",
+    college_name: "University of Texas at Austin",
+    public_private: "public",
+    city: "Austin",
+    state: "TX",
+    station_image:
+      "https://pbs.twimg.com/profile_images/1021906128332980224/tgMrKr7O_400x400.jpg",
+    college_image:
+      "https://assets-sports.thescore.com/basketball/team/1069/logo.png",
+    palette: ["#d46c2c", "#d86c2c", "#d86c2c", "#d86c2c"],
+  },
+  {
+    id: 6,
+    call_sign: "WSUM",
+    broadcast_frequency: "91.7",
+    audio_url: "https://ice23.securenetsystems.net/WSUMFM",
+    station_url: "https://wsum.org/",
+    college_name: "University of Wisconsin—Madison",
+    public_private: "public",
+    city: "Madison",
+    state: "WI",
+    station_image:
+      "https://www.radio.net/images/broadcasts/ec/fb/11375/c300.png",
+    college_image:
+      "https://www.badgerselect.com/assets/images/FAVIcon/android-chrome-256x256.png",
+    palette: ["#a30505", "#f9f7f7", "#0c0707", "#b17c7c"],
+  },
+  {
+    id: 7,
+    call_sign: "KCOU",
+    broadcast_frequency: "88.1",
+    audio_url: "https://ssl.shoutcaststreaming.us:8088/stream",
+    station_url: "https://kcou.fm/",
+    college_name: "University of Missouri",
+    public_private: "public",
+    city: "Columbia",
+    state: "MO",
+    station_image:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/Kcou-logo.jpg/200px-Kcou-logo.jpg",
+    college_image:
+      "https://assets-sports.thescore.com/basketball/team/684/logo.png",
+    palette: ["#edc96f", "#120f07", "#9d988a", "#7c7c7c"],
+  },
+];
+
+export default stations;
